a652d84529996e6703935c7165592fda0a86a2db
Emma Grunebaum <egrunebaum@Emmas-MacBook-Pro.local>
2018-11-20 18:20:01 -0500

    previous button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,31 +1,51 @@
 {
-$('button#play-pause').on('click', function() {
-  player.playPause();
-  $(this).attr('playState', player.playState);
-});
+  $('button#play-pause').on('click', function() {
+    player.playPause();
+    $(this).attr('playState', player.playState);
+  });
 
   $('button#next').on('click', function() {
-if (player.playState !== 'playing') { return; }
-const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-  });
-$('.button#previous').on('click', function() {
     if (player.playState !== 'playing') {
       return;
     }
-
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
-       const nextSong = album.songs[nextSongIndex];
-       player.playPause(nextSong);
-    };
-    $('button#previous').on('click', function(){
-      if (player.playState !== 'playing') { return; }
-
-      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-      const previousSongIndex = currentSongIndex - 1;
-      if (previousSongIndex <= -1) { return; }
-
-    const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
   });
+  $('.button#next').on('click', function() {
+        if (player.playState !== 'playing') {
+          return;
+        }
+
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const nextSongIndex = currentSongIndex + 1;
+        if (nextSongIndex >= album.songs.length) {
+          return;
+        }
+        const nextSong = album.songs[nextSongIndex];
+        player.playPause(nextSong);
+      };
+
+      $('#time-control input').on('input', function(event) {}); player.skipTo(event.target.value); setInterval(() => {
+        if (player.playState !== 'playing') {
+          return;
+        }
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime / duration) * 100;
+        $('#time-control .current-time').text(currentTime);
+        $('#time-control input').val(percent);
+      }, 1000);
+
+      $('button#previous').on('click', function() {
+        if (player.playState !== 'playing') {
+          return;
+        }
+
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const previousSongIndex = currentSongIndex - 1;
+        if (previousSongIndex <= -1) {
+          return;
+        }
+
+        const previousSong = album.songs[previousSongIndex];
+        player.playPause(previousSong);
+      });
